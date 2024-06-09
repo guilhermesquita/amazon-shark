a381c67c1657505672a3b8886af0a46686b248d1
4d4m0r <adamorbarbosa0@gmail.com>
2024-06-08 23:49:13 -0300

    correção do user

--- components/UserContent.tsx
+++ components/UserContent.tsx
@@ -1,17 +1,24 @@
-'use client';
+import { User } from '@supabase/supabase-js';
 import React, { useState, useEffect } from 'react';
 import { getUser } from './actions';
 import AddEmpresa from './addEmpresa/AddEmpresa';
 
+interface UserData {
+  user_metadata?: {
+    fullName?: string;
+  };
+}
+
 export default function UserContent() {
-  const [userFullName, setUserFullName] = useState('');
+  const [userFullName, setUserFullName] = useState<string | undefined>('');
 
   useEffect(() => {
     async function fetchUserData() {
       try {
-        const user = await getUser();
-        console.log(user)
-        setUserFullName(user.data?.user?.user_metadata.fullName);
+        const user: User | null = await getUser();
+        if (user && user.user_metadata && user.user_metadata.fullName) {
+          setUserFullName(user.user_metadata.fullName);
+        }
       } catch (error) {
         console.error('Erro ao buscar dados do usuário:', error);
       }
@@ -20,7 +27,5 @@ export default function UserContent() {
     fetchUserData();
   }, []);
 
-  return (
-    <AddEmpresa/>
-  );
+  return <AddEmpresa />;
 }
