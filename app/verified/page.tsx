791765723112f2a6c70d42a21964827f1813c1d5
guilhermesquita <guilhermer_mesquita@hotmail.com>
2024-07-29 16:53:25 -0400

    feat: create confirm payment

--- app/verified/page.tsx
+++ app/verified/page.tsx
@@ -13,7 +13,6 @@ export default function Index() {
   const { user } = useUser() as UserContextType;
 
   const [confirm, setConfirm] = useState(false);
-
   const router = useRouter();
 
   useEffect(() => {
@@ -62,10 +61,6 @@ export default function Index() {
     return null;
   };
 
-  useEffect(() => {
-    console.log(user);
-  });
-
   return (
     <section className="w-full">
       <header className="p-5 border-b border-b-foreground/10 w-full flex items-center h-14">
@@ -81,112 +76,112 @@ export default function Index() {
         </h1>
 
         <article>
-        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-          <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
-            Aumente suas possibilidades!
-          </h5>
-          <div className="flex items-baseline text-gray-900 dark:text-white">
-            <span className="text-3xl font-semibold">R$</span>
-            <span className="text-5xl font-extrabold tracking-tight">57</span>
-            <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
-              /mês
-            </span>
+          <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
+              Aumente suas possibilidades!
+            </h5>
+            <div className="flex items-baseline text-gray-900 dark:text-white">
+              <span className="text-3xl font-semibold">R$</span>
+              <span className="text-5xl font-extrabold tracking-tight">57</span>
+              <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+                /mês
+              </span>
+            </div>
+            <ul role="list" className="space-y-5 my-7">
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                  Credibilidade e confiança
+                </span>
+              </li>
+              <li className="flex">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                  Maior visibilidade
+                </span>
+              </li>
+              <li className="flex">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                  Engajamento Aprimorado
+                </span>
+              </li>
+              <li className="flex">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                  Proteção contra intimidadores
+                </span>
+              </li>
+              <li className="flex line-through decoration-gray-500">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 ms-3">
+                  Plágios de sua empresa
+                </span>
+              </li>
+              <li className="flex line-through decoration-gray-500">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 ms-3">
+                  Insegurança
+                </span>
+              </li>
+            </ul>
+            <button
+              type="button"
+              onClick={() => setConfirm(true)}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+            >
+              Eu quero ser um verificado!
+            </button>
           </div>
-          <ul role="list" className="space-y-5 my-7">
-            <li className="flex items-center">
-              <svg
-                className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-              </svg>
-              <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-                Credibilidade e confiança
-              </span>
-            </li>
-            <li className="flex">
-              <svg
-                className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-              </svg>
-              <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-                Maior visibilidade
-              </span>
-            </li>
-            <li className="flex">
-              <svg
-                className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-              </svg>
-              <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-                Engajamento Aprimorado
-              </span>
-            </li>
-            <li className="flex">
-              <svg
-                className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-              </svg>
-              <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-                Proteção contra intimidadores
-              </span>
-            </li>
-            <li className="flex line-through decoration-gray-500">
-              <svg
-                className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-              </svg>
-              <span className="text-base font-normal leading-tight text-gray-500 ms-3">
-                Plágios de sua empresa
-              </span>
-            </li>
-            <li className="flex line-through decoration-gray-500">
-              <svg
-                className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-              </svg>
-              <span className="text-base font-normal leading-tight text-gray-500 ms-3">
-                Insegurança
-              </span>
-            </li>
-          </ul>
-          <button
-            type="button"
-            onClick={() => setConfirm(true)}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
-          >
-            Eu quero ser um verificado!
-          </button>
-        </div>
-      </article>
+        </article>
         <article className="flex justify-center mb-5">
           {callbackQrCode()}
         </article>
--- components/qrcodeField/qrCodeField.tsx
+++ components/qrcodeField/qrCodeField.tsx
@@ -1,20 +1,35 @@
 import { useState, useEffect } from "react";
 import ToastedCopied from "../toastCopied/ToastCopied";
-import { createPaymentPix, requestBodyPost } from "@/service/pixApi";
+import {
+  createPaymentPix,
+  getPaymentPix,
+  requestBodyPost,
+} from "@/service/pixApi";
 import Spinner from "../Spinner/Spinner";
+import { FaCheckCircle } from "react-icons/fa";
+import { editVerificationById } from "../actions";
+import { useRouter } from "next/navigation";
+import { UserContextType, useUser } from "@/app/context/userContext";
 
 interface PixModel {
   qrCodeBase64: string;
   qrCode: string;
 }
 
-interface createPaymentPixProps{
+interface createPaymentPixProps {
   request: requestBodyPost;
 }
 
-export default function QrCodeField({request}: createPaymentPixProps) {
+export default function QrCodeField({ request }: createPaymentPixProps) {
+
+  const router = useRouter();
+
   const [toast, setToast] = useState(false);
   const [qrCodePix, setQrCodePix] = useState<PixModel | undefined>();
+  const [idPayment, setIdPayment] = useState(0);
+  const [approvedPayment, setApprovedPayment] = useState(false);
+
+  const {user} = useUser() as UserContextType
 
   useEffect(() => {
     (async () => {
@@ -24,8 +39,9 @@ export default function QrCodeField({request}: createPaymentPixProps) {
           qrCodeJson.point_of_interaction.transaction_data.qr_code_base64,
         qrCode: qrCodeJson.point_of_interaction.transaction_data.qr_code,
       });
-    })();
 
+      setIdPayment(qrCodeJson.id);
+    })();
     if (toast) {
       const timer = setTimeout(() => {
         setToast(false);
@@ -47,20 +63,47 @@ export default function QrCodeField({request}: createPaymentPixProps) {
     setToast(true);
   };
 
+  useEffect(() => {
+    const intervalId = setInterval(async () => {
+      if (qrCodePix !== undefined) {
+        const getPixPaymentConfirmation = await getPaymentPix(idPayment);
+        console.log(getPixPaymentConfirmation.status);
+        if (getPixPaymentConfirmation.status === "approved") {
+          clearInterval(intervalId);
+          setApprovedPayment(true)
+          const idClient = user?.id as string
+          await editVerificationById(idClient)
+          router.push('/')
+        }
+      }
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [qrCodePix, idPayment]);
+
   return qrCodePix ? (
     <section className="w-3/4 border-2 border-white flex flex-col items-center p-10 gap-10">
-      <img
-        src={`data:image/jpeg;base64, ${qrCodePix?.qrCodeBase64}`}
-        width="400" className="rounded-lg"
-      />
-      <button
-        onClick={copyText}
-        className="py-2 px-3 flex rounded-md no-underline 
+      {approvedPayment ? (
+        <div className="flex items-center gap-5 h-full w-full">
+        <FaCheckCircle size={50} color="#1BB947"/>  Pagamento Aprovado!
+        </div>
+      ) : (
+        <div className="h-full w-full flex flex-col justify-center items-center gap-4">
+          <img
+            src={`data:image/jpeg;base64, ${qrCodePix?.qrCodeBase64}`}
+            width="400"
+            className="rounded-lg"
+          />
+          <button
+            onClick={copyText}
+            className="py-2 px-3 flex rounded-md no-underline 
         bg-btn-background hover:bg-btn-background-hover"
-      >
-        copiar código QRCODE
-      </button>
-      {toast && <ToastedCopied />}
+          >
+            copiar código QRCODE
+          </button>
+          {toast && <ToastedCopied />}
+        </div>
+      )}
     </section>
   ) : (
     <div className="p-10">
