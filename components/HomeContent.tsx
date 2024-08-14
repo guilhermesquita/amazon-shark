e1ddae065dfff3ab41fa0be8f8b763c96059011a
guilherme mesquita <guilherme.mesquita@agrosmart.com.br>
2024-08-14 11:59:34 -0400

    refactor: adjustment responsive mode

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 function HomeContent() {
   return (
-    <main className="hero flex flex-col items-center justify-center px-20 text-center">
-      <h1 className="text-6xl font-normal text-[#fff]">
+    <main className="hero flex flex-col items-center justify-center px-5 lg:px-20 text-center">
+      <h1 className="text-4xl lg:text-6xl font-normal text-[#fff]">
         Prazer, somos a Amazon Shark
       </h1>
       <p className="font-normal text-[#fff]">navegue nesse rio de oportunidades</p>
