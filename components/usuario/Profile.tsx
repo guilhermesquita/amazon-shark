675d9e343d7113980121a24196a983356a998929
guilherme mesquita <guilherme.mesquita@agrosmart.com.br>
2024-08-15 17:31:49 -0400

    feat: style base

--- components/usuario/Profile.tsx
+++ components/usuario/Profile.tsx
@@ -1,12 +1,30 @@
-// Profile.js
-import React from 'react';
+import { ClientContextType, useClient } from "@/app/context/clientContext";
+import { UserContextType, useUser } from "@/app/context/userContext";
+import React from "react";
 
 const Profile = () => {
+  const { client } = useClient() as ClientContextType;
+  const { user } = useUser() as UserContextType;
+
   return (
-    <div className="p-4 border rounded-lg shadow-md h-full">
-      <h2 className="text-2xl font-semibold mb-4">Perfil</h2>
-      <p>Informações do usuário aqui.</p>
-    </div>
+    <article className="p-4 border rounded-lg shadow-md h-full flex flex-col">
+      <div>
+        <h2 className="text-2xl font-semibold mb-4">Meu Perfil</h2>
+      </div>
+      <section className="flex gap-5">
+        <h2 className="w-32 h-32 bg-gray-400 rounded flex items-center justify-center flex-shrink-0">
+          {client?.full_name[0].toUpperCase()}
+        </h2>
+        <article className="flex flex-col gap-5">
+          <div className="flex flex-col bg-slate-400 p-2 rounded">
+           <p>{client?.full_name}</p> 
+           <p className="text-xs">nome completo</p> 
+          </div>
+          <p>{user?.email}</p>
+          <p>alterar senha</p>
+        </article>
+      </section>
+    </article>
   );
 };
 
